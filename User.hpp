1e090176f6def04dccd07cc86ee262a6b5cba5a2
Ullhor Greyiron <ugreyiro@si-f1.kzn.21-school.ru>
2021-05-10 16:38:15 +0300

    edit user.hpp

--- User.hpp
+++ User.hpp
@@ -6,13 +6,21 @@
 #define FT_IRC_USER_HPP
 
 #include <string>
+#include "Server.hpp"
 
 class User
 {
 private:
-	int			id_;
+	//int			id_;
 	std::string	nick_;
-	std::string	name_;
+	std::string	userName_;
+	std::string realName_;
+	std::string password_;
+	int 		mod_;
+	bool 		isOper_;
+	std::string netwideId_;
+	Server&		server_;
+	int 		socket_;
 
 	/// Unused constructors
 	User() {};
@@ -20,16 +28,33 @@ private:
 	User& operator= (const User& other) { return *this; };
 
 public:
-	User(int id, const std::string& nick) : id_(id), nick_(nick) {};
+	User(int socket, const Server& server, int mod, const std::string& userName, const std::string& realName)
+		: socket_(socket), server_(server), mod_(mod), userName_(userName), realName_(realName)
+	{
+		isOper_ = false;
+		password_ = false;
+	};
+
 	~User() {};
 
-	void		set_id		(int id) { id_ = id; }
-	void		set_nick	(const std::string& nick) { nick_ = nick; }
-	void		set_name	(const std::string& name) { name_ = name; }
+	void		setNick			(const std::string& nick)		{ nick_ = nick; }
+	void		setUserName		(const std::string& userName)	{ userName_ = userName; }
+	void 		setRealName 	(const std::string& realName)	{ realName_ = realName; }
+	void 		setPassword		(const std::string& password)	{ password_ = password; }
+	void 		setMod			(int mod)						{ mod_ = mod; }
+	void 		setIsOper		(bool isOper)					{ isOper_ = isOper; }
+	void 		setNetwideId	(const std::string& netWideId)	{ netWideId_ = netWideId; }
 
-	int			id			() const { return id_; }
-	std::string nick		() const { return nick_; }
-	std::string name		() const { return name_; }
+	const std::string&	nick		() const { return nick_; }
+	const std::string&	userName	() const { return userName_; }
+	const std::string&	realName	() const { return realName_; }
+	const std::string&	password	() const { return password_; }
+	int 				mod			() const { return mod_; }
+	bool 				isOper		() const { return isOper_; }
+	const std::string&	netwideId	() const { return netwideId_; }
+	const Server&		server		() const { return server_; }
+	int					socket		() const {return socket_; }
+
 
 };
 
