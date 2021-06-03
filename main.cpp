8ede3e9cd3e7e18dd894c42c439c7498b1b314f1
pkentaur <n.smolianitski@gmail.com>
2021-06-03 15:22:22 +0300

    Fix main

--- main.cpp
+++ main.cpp
@@ -9,13 +9,10 @@ void server_cycle(const std::string& host, int port_network, const std::string& 
 {
 	try
 	{
-		//! NGIRCD TESTS
-//		Irisha server("et-a6", 6667, "psw", 6668, "psw");
-
-		//! IRISHA TESTS
-		Irisha server(6667, "psw");
-//		Irisha server("localhost", 8000, "psw", 7000, "psw");
-//		Irisha server("localhost", 7000, "psw", 6000, "psw");
+		if (!host.empty() && port_network && !password_network.empty() && port && !password.empty())
+			Irisha server(host, port_network, password_network, port, password);
+		else
+			Irisha server(port, password);
 	}
 	catch (std::exception& e)
 	{
@@ -31,14 +28,20 @@ int	main(int argc, char *argv[])
     std::string	password_network;
     int			port;
     std::string	password;
-
-//    parse_argv(argc, argv, host, port_network, password_network, port, password);
-//    std::cout << GREEN "HOST             = " CLR<< host << std::endl;
-//    std::cout << GREEN "PORT NETWORK     = " CLR<< port_network << std::endl;
-//    std::cout << GREEN "PASSWORD NETWORK = " CLR<< password_network << std::endl;
-//    std::cout << GREEN "PORT             = " CLR << port << std::endl;
-//    std::cout << GREEN "PASSWORD         = " CLR << password << std::endl;
+	try
+	{
+    	parse_argv(argc, argv, host, port_network, password_network, port, password);
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << RED BOLD E_SCULL " ALARM! " << e.what() << " " E_SCULL CLR << std::endl;
+		exit(1);
+	}
+    std::cout << GREEN "HOST             = " CLR<< host << std::endl;
+    std::cout << GREEN "PORT NETWORK     = " CLR<< port_network << std::endl;
+    std::cout << GREEN "PASSWORD NETWORK = " CLR<< password_network << std::endl;
+    std::cout << GREEN "PORT             = " CLR << port << std::endl;
+    std::cout << GREEN "PASSWORD         = " CLR << password << std::endl;
 	server_cycle(host, port_network, password_network, port, password);
 	return 0;
 }
-
